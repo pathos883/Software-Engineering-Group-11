7798b856a137013a701a7d14e1c5c6ca4aca9507
omegadude1 <isaac.t.thompson@gmail.com>
2019-05-01 15:23:12 -0400

    initial version of csv export, is not tested

--- DoctorSchedulerJava/src/DoctorSchedule.java
+++ DoctorSchedulerJava/src/DoctorSchedule.java
@@ -4,6 +4,8 @@ import org.optaplanner.core.api.domain.solution.PlanningSolution;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 @PlanningSolution
 public class DoctorSchedule {
@@ -25,6 +27,61 @@ public class DoctorSchedule {
 
     //function to export a csv
     void exportCSV(String fileName){
-
+    	PrintWriter writer = new PrintWriter("calendar.csv");
+    	
+    	writer.println("Subject, Start Date, Start Time, End Date, End Time");
+    	
+    	for (Shift s : shifts) {
+    		LocalDateTime startTime = s.getStartTime();
+    		LocalDateTime endTime = s.getEndTime();
+    		
+    		// write name of doctor
+    		writer.print(s.getShiftFile().getName());
+    		writer.print(",");
+    		
+    		// write start date
+    		writer.print(startTime.getMonthValue() + "/" + startTime.getDayOfMonth() + "/" + startTime.getYear());
+    		writer.print(",");
+    		
+    		// write start tiem
+    		int hour = startTime.getHour();
+    		boolean pm = (hour / 12 == 1);
+    		hour = hour % 12;
+    		if (hour == 0) {
+    			writer.print("12");
+    		} else {
+    			writer.print(hour);
+    		}
+    		writer.print(":");
+    		writer.print(startTime.getMinute());
+    		if (pm) {
+    			writer.print("PM");
+    		} else {
+    			writer.print("AM");
+    		}
+    		writer.print(",");
+    		
+    		// write end date
+    		writer.print(endTime.getMonthValue() + "/" + endTime.getDayOfMonth() + "/" + endTime.getYear());
+    		writer.print(",");
+    		
+    		// write end tiem
+    		hour = endTime.getHour();
+    		pm = (hour / 12 == 1);
+    		hour = hour % 12;
+    		if (hour == 0) {
+    			writer.print("12");
+    		} else {
+    			writer.print(hour);
+    		}
+    		writer.print(":");
+    		writer.print(endTime.getMinute());
+    		if (pm) {
+    			writer.print("PM");
+    		} else {
+    			writer.print("AM");
+    		}
+    		writer.println();
+    	}
     }
 }
